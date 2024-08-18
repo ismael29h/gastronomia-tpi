ce08de277a421ded348cde09b3cf5a89ba63f211
HERNAN Ismael Norberto Jesús <ismael29h@gmail.com>
2024-08-18 10:58:31 -0300

    Entidad creada: Evento

---
+++ src/main/java/com/gastronomia/domain/Evento.java
@@ -0,0 +1,83 @@
+package com.gastronomia.domain;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class Evento {
+    private UUID id;
+    private String nombre;
+    private String descripcion;
+    private LocalDateTime fechaYHora;
+    private String ubicacion;
+    private int capacidad;
+    private Chef chef;
+
+    // constructores
+    public Evento(String nombre, String descripcion, LocalDateTime fechaYHora, String ubicacion, int capacidad,
+            Chef chef) {
+        this.id = UUID.randomUUID();
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.fechaYHora = fechaYHora;
+        this.ubicacion = ubicacion;
+        this.capacidad = capacidad;
+        this.chef = chef;
+    }
+
+    // getters&setters
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public LocalDateTime getFechaYHora() {
+        return fechaYHora;
+    }
+
+    public void setFechaYHora(LocalDateTime fechaYHora) {
+        this.fechaYHora = fechaYHora;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public Chef getChef() {
+        return chef;
+    }
+
+    public void setChef(Chef chef) {
+        this.chef = chef;
+    }
+}
