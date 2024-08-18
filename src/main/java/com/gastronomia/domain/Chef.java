0cdefeacb766870805b73fe2b02a5ab8c573b6d5
HERNAN Ismael Norberto Jesús <ismael29h@gmail.com>
2024-08-18 10:58:10 -0300

    Entidad creada: Chef

---
+++ src/main/java/com/gastronomia/domain/Chef.java
@@ -0,0 +1,52 @@
+package com.gastronomia.domain;
+
+import java.util.List;
+
+import com.gastronomia.enumeration.Especialidad;
+
+public class Chef {
+    private int dni;
+    private String nombre;
+    private Especialidad especialidad;
+    private List<Evento> eventos;
+
+    // constructores
+    public Chef(int dni, String nombre, Especialidad especialidad) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.especialidad = especialidad;
+    }
+
+    // getters&setters
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Especialidad getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(Especialidad especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    public List<Evento> getEventos() {
+        return eventos;
+    }
+
+    public void setEventos(List<Evento> eventos) {
+        this.eventos = eventos;
+    }
+}
