f4177aa8a625160a3b541772b6d00b618bcd00bc
HERNAN Ismael Norberto Jesús <ismael29h@gmail.com>
2024-08-18 16:06:38 -0300

    Lógica de negocio: Menu

---
+++ src/main/java/com/gastronomia/service/menu/MenuService.java
@@ -0,0 +1,5 @@
+package com.gastronomia.service.menu;
+
+public interface MenuService {
+    public void mostrarMenu();
+}
---
+++ src/main/java/com/gastronomia/service/menu/impl/MenuServiceImpl.java
@@ -0,0 +1,20 @@
+package com.gastronomia.service.menu.impl;
+
+import java.util.Scanner;
+
+import com.gastronomia.service.menu.MenuService;
+
+public class MenuServiceImpl implements MenuService {
+    private Scanner sc;
+
+    public MenuServiceImpl(Scanner sc) {
+        this.sc = sc;
+    }
+
+    @Override
+    public void mostrarMenu() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
